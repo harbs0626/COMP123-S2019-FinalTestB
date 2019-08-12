f319c6b68c0cc6a945e751e0a21ecaaa30beb8ce
harbs0626 <harbinramo@yahoo.com>
2019-08-12 12:53:06 -0400

    Added Character and Item Classes

---
+++ COMP123-S2019-FinalTestB/Objects/Character.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Student Name: Harbin Ramo
+ * Student ID  : 301046044
+ * Description : This is the Character class used in character creation
+ *               This is also the Data container for the application 
+ */
+
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    class Character
+    {
+        //Character Abilities
+        public string Strength { get; set; }
+        public string Dexterity { get; set; }
+        public string Constitution { get; set; }
+        public string Intelligence { get; set; }
+        public string Wisdom { get; set; }
+        public string Charisma { get; set; }
+
+        //Secondary Abilities
+        public int ArmourClass { get; set; }
+        public int HitPoints { get; set; }
+
+        //Character Class
+        public string CharacterClass { get; set; }
+        public int Level { get; set; }
+
+        //Equipments
+        List<Item> Inventory;
+
+        //Constructor
+        Character()
+        {
+            this.Inventory = new List<Item>();
+
+        }
+    }
+}
